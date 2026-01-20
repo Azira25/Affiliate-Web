8e34ea794b258be7efc70a2f03e3a804c3892f86
Azira25 <azisnewer@gmail.com>
2026-01-20 10:43:39 +0700

    Update and rename config.example.js to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// File ini menyimpan konfigurasi rahasia.
+// JANGAN upload file ini ke GitHub.
+
+const ADMIN_CONFIG = {
+    // Ganti 'admin123' dengan password rahasia pilihan Anda
+    password: "Ketoprak123" 
+};
